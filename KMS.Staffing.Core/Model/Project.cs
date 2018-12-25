aecf7b8d156a9c18f60d88b1212748aa7ba57744
KMS\khoimnguyen <khoimnguyen@kms-technology.com>
2018-12-25 21:56:43 +0700

    add repo and unit test

---
+++ KMS.Staffing.Core/Model/Project.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMS.Staffing.Core.Model
+{
+    [Table("Project")]
+    public class Project
+    {        
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Client { get; set; }
+
+        public string Photo { get; set; }
+
+        public string Description { get; set; }
+
+        public int TeamSize { get; set; }
+
+        public Guid DepartmentId { get; set; }
+
+        public int Status { get; set; }
+    }
+}
--- KMS.Staffing.Logic/ProjectsLogic.cs
+++ KMS.Staffing.Logic/ProjectsLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KMS.Staffing.Core.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,21 @@ namespace KMS.Staffing.Logic
 {
     public class ProjectsLogic : IProjectsLogic
     {
+        readonly IProjectRepository projectRepository;
+
+        public ProjectsLogic(IProjectRepository projectRepository)
+        {
+            this.projectRepository = projectRepository;
+        }
+
         public string test(string name)
         {
             return $"Hello {name}";
+        }
+
+        public int CountProjects()
+        {
+            return projectRepository.GetProjects().Count();            
         }
     }
 }
