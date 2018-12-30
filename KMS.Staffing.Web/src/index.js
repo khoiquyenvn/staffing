98e03ba58b14d2ff69ca38b8657a68904fe210aa
KMS\trangha <trangha@kms-technology.com>
2018-12-30 17:38:27 +0700

    Resolve conflicts in index file

--- KMS.Staffing.Web/src/index.js
+++ KMS.Staffing.Web/src/index.js
@@ -6,14 +6,10 @@ import { Provider } from 'react-redux'
 import configureStore from './store/configureStore';
 import App from './App'
 import 'react-table/react-table.css'
-import {loadEmployees} from './actions/employeeActions';
-import {loadTitles} from './actions/TitleActions';
 import { syncHistoryWithStore} from 'react-router-redux';
+import { createBrowserHistory } from 'history';
 
 const store = configureStore();
-store.dispatch(loadEmployees());
-store.dispatch(loadTitles());
-import { createBrowserHistory } from 'history';
 
 const browserHistory = createBrowserHistory();
 const history = syncHistoryWithStore(browserHistory, store);
